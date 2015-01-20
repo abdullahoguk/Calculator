c737d338f34d6c477f0d08f8cb6a70a59c1d5eb7
Abdullah <aoguk1102@gmail.com>
2015-01-20 22:20:35 +0200

    Create EmptyQueueException.java

---
+++ EmptyQueueException.java
@@ -0,0 +1,16 @@
+/**
+* Celal Bayar University - Department of Computer Engineering
+* CSE 2105 Data Structures 
+* Project I
+*
+* Authors: Abdullah ÖĞÜK 120315030
+*        Ahmet KAŞİF 130315012
+* Date: NOV2014
+*/ 
+
+
+public class EmptyQueueException extends RuntimeException {  
+  public EmptyQueueException(String err) {
+    super(err);
+  }
+}
