032fc574199440340563d99f59fab83b86fdb68b
Abdullah <aoguk1102@gmail.com>
2015-01-21 22:51:26 +0200

    Create EmptyStackException.java

---
+++ EmptyStackException.java
@@ -0,0 +1,15 @@
+/**
+* Celal Bayar University - Department of Computer Engineering
+* CSE 2105 Data Structures 
+* Project I
+*
+* Authors: Abdullah ÖĞÜK 120315030
+*        Ahmet KAŞİF 130315012
+* Date: NOV2014
+*/ 
+
+public class EmptyStackException extends RuntimeException {  
+  public EmptyStackException(String err) {
+    super(err);
+  }
+}
