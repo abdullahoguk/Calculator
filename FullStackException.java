d556c1fd66474a812f029c451353317cec61f5ed
Abdullah <aoguk1102@gmail.com>
2015-01-23 13:17:06 +0200

    Create FullStackException.java

---
+++ FullStackException.java
@@ -0,0 +1,15 @@
+/**
+* Celal Bayar University - Department of Computer Engineering
+* CSE 2105 Data Structures 
+* Project I
+*
+* Authors: Abdullah ÖĞÜK 120315030
+*        Ahmet KAŞİF 130315012
+* Date: NOV2014
+*/ 
+
+public class FullStackException extends RuntimeException {  
+  public FullStackException(String err) {
+    super(err);
+  }
+}
